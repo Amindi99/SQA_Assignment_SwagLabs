cab9b99408c8a109aeb055bbf966dac4d2b354b0
Amindi99 <amindiperera1@gmail.com>
2024-06-22 10:50:28 +0530

    Test case section added

--- src/test/java/SwagLabs_Assignment/OnlineShoppingCart.java
+++ src/test/java/SwagLabs_Assignment/OnlineShoppingCart.java
@@ -1,4 +1,37 @@
 package SwagLabs_Assignment;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.apache.commons.io.FileUtils;
+import org.testng.annotations.Test;
+
 public class OnlineShoppingCart {
+
+    //Global variable section
+    String BaseURL = "https://www.saucedemo.com/";
+    WebDriver driver;
+    String actualText;
+    String expectedText;
+    Boolean status;
+
+    //Before test section
+    @BeforeTest
+    public void BeforeTestMethod(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    //Test case section
+
+
+    //After test section
+
+
+    //Supportive method section
+
+
 }
